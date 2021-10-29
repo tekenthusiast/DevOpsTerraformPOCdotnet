34b6b24375222bd9d6c3c67ec22fa883d57dbc6c
tekenthusiast <vamshigokulreddy@gmail.com>
2021-10-29 00:31:33 -0500

    Changing controllers weatherforecast XXXXX

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace weatherapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "xxxxxx", "xxxxxx", "xxxxxx", "xxxxxx", "xxxxxx", "xxxxxx", "xxxxxx", "xxxxxx", "xxxxxx", "xxxxxx"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
